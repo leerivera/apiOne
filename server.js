17424400d0c6c96997253a11a1dd1c5e9922ed78
leerivera <nick@MSI.localdomain>
2022-06-20 18:32:43 -0400

    more server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) =>{
+    res.send('Boo ya!')
+})
+
 
 app.listen(3000, function(){
     console.log('3000 you heard?!')
