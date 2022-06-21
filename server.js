eadae569fc920bc356b975937a1028fe8396d6f8
leerivera <nick@MSI.localdomain>
2022-06-21 14:01:51 -0400

    mongo

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient
+
+// place this before all crud code it tells bp to extract data form and add it to req
 app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/index.html')
 })
 app.post('/quotes', (req, res) =>{
-    console.log("we posted")
+    console.log(req.body)
 })
 
 app.listen(3000, function(){
