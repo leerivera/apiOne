7b753325de0750edb184a27768304255f3e11cf5
leerivera <leerivera80@gmail.com>
2022-06-22 19:24:08 -0400

    connect

--- server.js
+++ server.js
@@ -1,21 +1,60 @@
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://nick:<password>@cluster0.1y9w1mw.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
 const express = require('express');
 const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 const app = express();
 
-const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb-connection-string', (err, client) =>{
+
+const connectionString = 'mongodb+srv://nick:password3177@cluster0.1y9w1mw.mongodb.net/test?retryWrites=true&w=majority'
+
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true})
+.then(client => {
+    console.log('database is connected like 4')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+    app.use(bodyParser.urlencoded({ extended: true}))
     
+    app.get('/', (req, res) =>{
+        res.sendFile(__dirname + '/index.html')
+    })
+    
+     app.post('/quotes', (req, res) => {
+         quotesCollection.insertOne(req.body)
+           .then(result => {
+             console.log(result)
+           })
+           .catch(error => console.error(error))
+      })
+    // app.listen(/** */)
 })
+.catch(console.error)
 // place this before all crud code it tells bp to extract data form and add it to req
-app.use(bodyParser.urlencoded({ extended: true}))
+
 
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/index.html')
 })
-app.post('/quotes', (req, res) =>{
-    console.log(req.body)
-})
+
+// app.post('/quotes', (req, res) => {
+//     quotesCollection.insertOne(req.body)
+//       .then(result => {
+//         console.log(result)
+//       })
+//       .catch(error => console.error(error))
+//   })
 
 app.listen(3000, function(){
     console.log('3000 you heard?!')
